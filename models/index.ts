a22892098821341342d965b77b94a76fa9570958
Fikcup <rhys.n.wright@gmail.com>
2021-10-23 08:45:28 -0700

    feat : added one to many relationship from User to Question and Answerr

--- models/index.ts
+++ models/index.ts
@@ -2,4 +2,7 @@ const User = require('./User');
 const Question = require('./Question');
 const Answer = require('./Answer');
 
+User.hasMany(Answer);
+User.hasMany(Question);
+
 module.exports = { User, Question, Answer };
