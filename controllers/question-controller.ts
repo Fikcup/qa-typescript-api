b96703b48a1f1593399edb9711952b5e34212537
Fikcup <rhys.n.wright@gmail.com>
2021-10-23 09:45:58 -0700

    feat : create question

--- controllers/question-controller.ts
+++ controllers/question-controller.ts
@@ -2,9 +2,21 @@ export {};
 import { Request, Response } from 'express';
 const { Question } = require('../models');
 
+// TODO : Create a user association with the question
+
 const questionController = {
     getAllQuestions(req: Request, res: Response) {
         Question.findAll()
+            .then((questionData: Object) => {
+                res.json(questionData);
+            })
+            .catch((err: any) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+    createQuestion(req: Request, res: Response) {
+        Question.create(req.body)
             .then((questionData: Object) => {
                 res.json(questionData);
             })
--- routes/api/question-routes.ts
+++ routes/api/question-routes.ts
@@ -2,9 +2,10 @@ export {};
 const router = require('express').Router();
 const {
     getAllQuestions,
+    createQuestion,
 } = require('../../controllers/question-controller');
 
 // route /api/questions/
-router.route('/').get(getAllQuestions);
+router.route('/').get(getAllQuestions).post(createQuestion);
 
 module.exports = router;
