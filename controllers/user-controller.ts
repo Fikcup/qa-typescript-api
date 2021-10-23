631390de92d267b56a908b44891436037883ec98
Fikcup <rhys.n.wright@gmail.com>
2021-10-23 09:10:50 -0700

    feat : get one user

--- controllers/user-controller.ts
+++ controllers/user-controller.ts
@@ -13,6 +13,20 @@ const userController = {
                 res.status(500).json(err);
             });
     },
+    getOneUser(req: Request, res: Response) {
+        User.findOne({
+            where: {
+                uuid: req.params.userId
+            }
+        })
+            .then((userData: Object) => {
+                res.json(userData);
+            })
+            .catch((err: any) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
     createUser(req: Request, res: Response) {
         User.create(req.body)
             .then((userData: Object) => {
--- routes/api/user-routes.ts
+++ routes/api/user-routes.ts
@@ -2,10 +2,14 @@ export {};
 const router = require('express').Router();
 const {
     getAllUsers,
+    getOneUser,
     createUser,
 } = require('../../controllers/user-controller');
 
 // route /api/users/
 router.route('/').get(getAllUsers).post(createUser);
+
+// route /api/user/:userId
+router.route('/:userId').get(getOneUser);
 
 module.exports = router;
