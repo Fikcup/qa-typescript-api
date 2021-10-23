8ab4ed32bb8756e8dbf03afa81430919aa8ebc4e
Fikcup <rhys.n.wright@gmail.com>
2021-10-23 08:44:14 -0700

    feat : Answer model

--- models/Answer.ts
+++ models/Answer.ts
@@ -5,11 +5,36 @@ const sequelize = require('../config/connection');;
 
 class Answer extends Model {}
 
-Answer.init({
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'answer',
-});
+Answer.init(
+    {
+        uuid: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV1,
+            primaryKey: true,
+            allowNull: false,
+            unique: true,
+        },
+        voteCount: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0,
+            allowNull: false,
+        },
+        chosenAnswer: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: 0,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'answer',
+    }
+);
 
 module.exports = Answer;
