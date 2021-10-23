ce52e7f6d4849e262d3179907c477abdd6fcbeaa
Fikcup <rhys.n.wright@gmail.com>
2021-10-23 08:41:11 -0700

    feat : question model

--- models/Question.ts
+++ models/Question.ts
@@ -5,11 +5,31 @@ const sequelize = require('../config/connection');
 
 class Question extends Model {}
 
-Question.init({
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'question',
-});
+Question.init(
+    {
+        uuid: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV1,
+            primaryKey: true,
+            allowNull: false,
+            unique: true,
+        },
+        voteCount: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'question',
+    }
+);
 
 module.exports = Question;
