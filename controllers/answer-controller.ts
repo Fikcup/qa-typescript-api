c29b4474c97837ab9415eb0a679b883bfe4a3899
Fikcup <rhys.n.wright@gmail.com>
2021-10-23 10:08:09 -0700

    feat : get one answer

--- controllers/answer-controller.ts
+++ controllers/answer-controller.ts
@@ -2,9 +2,26 @@ export {};
 import { Request, Response } from 'express';
 const { Answer } = require('../models');
 
+// TODO : Create a user association with the answer
+// TODO : Create a question association with the answer
+
 const answerController = {
     getAllAnswers(req: Request, res: Response) {
         Answer.findAll()
+            .then((answerData: Object) => {
+                res.json(answerData);
+            })
+            .catch((err: any) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+    getOneAnswer(req: Request, res: Response) {
+        Answer.findOne({
+            where: {
+                uuid: req.params.answerId
+            }
+        })
             .then((answerData: Object) => {
                 res.json(answerData);
             })
--- routes/api/answer-routes.ts
+++ routes/api/answer-routes.ts
@@ -2,10 +2,14 @@ export {};
 const router = require('express').Router();
 const {
     getAllAnswers,
+    getOneAnswer,
     createAnswer,
 } = require('../../controllers/answer-controller');
 
 // route /api/answers/
 router.route('/').get(getAllAnswers).post(createAnswer);
+
+// route /api/answers/:answerId
+router.route('/:answerId').get(getOneAnswer);
 
 module.exports = router;
